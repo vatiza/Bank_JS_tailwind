3a9ffdf512e051b8e9a7fa44ab60a153455d8f85
vatiza <rakibur.mac@gmail.com>
2023-02-16 19:16:30 +0600

    bank.html add

--- js/login.js
+++ js/login.js
@@ -1,3 +1,19 @@
+
 document.getElementById('btn-submit').addEventListener('click', function () {
-    console.log('submit button clicked');
+  // emailField  
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    console.log(email)
+
+    // getpss
+    const passField = document.getElementById('user-pass');
+    const pass = passField.value;
+    console.log(pass);
+    // verify 
+    if (email === 'admin@vatiza.com' && pass === 'admin') {
+        console.log('valid user');
+    } else {
+        console.log('invalid user')
+    }
+
 })
