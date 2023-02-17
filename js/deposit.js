fdb47be784a23a7a9125413b2038a9260cf91029
vatiza <rakibur.mac@gmail.com>
2023-02-17 22:20:13 +0600

    exit

--- js/deposit.js
+++ js/deposit.js
@@ -2,10 +2,16 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
   const depositField=document.getElementById('deposit-input');
   const newDepositAmountString = depositField.value;
   const newDepositAmount = parseFloat(newDepositAmountString);
+  depositField.value='';
+  if (isNaN(newDepositAmount)) {
+    alert('Please Input Number')
+    return;
+}
 
   const depositTotalElement=document.getElementById('total-deposit');
   const previousDepositTotalString = depositTotalElement.innerText;
-  const previousDepositTotal=parseFloat(previousDepositTotalString)
+  const previousDepositTotal = parseFloat(previousDepositTotalString)
+  
   const currentDepositTotal = previousDepositTotal + newDepositAmount;
   depositTotalElement.innerText = currentDepositTotal;
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,20 +2,32 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-input');
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    withdrawField.value = '';
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please Input Number')
+        return;
+    }
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    
 
     const balanceTotalElement = document.getElementById('total-balance');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+ 
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Amar kache Eto Taka Nei');
+        return;
+    }
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    withdrawField.value = '';
-
+   
 })
